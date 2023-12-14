57350e761031aad9ed6a65d25ff988453d5ea62c
luhua <luhuadream@foxmail.com>
2023-12-14 11:50:20 +0800

    设置标题界面大小

--- src/main/kotlin/io/github/lhDream/view/TitleView.kt
+++ src/main/kotlin/io/github/lhDream/view/TitleView.kt
@@ -9,14 +9,27 @@ import kotlin.system.exitProcess
 class TitleView: View() {
 
     override val root = anchorpane{
+        prefWidth = 900.0
+        prefHeight = 600.0
         imageview("images/title.png"){
             fitWidth = 600.0
             fitHeight = 400.0
         }
         vbox {
+            spacing = 20.0
             layoutX = 150.0
             layoutY = 500.0
-            button("开始游戏") {
+            button("单人模式") {
+                action {
+                    replaceWith<MainView>()
+                }
+            }
+            button("双人模式") {
+                action {
+                    replaceWith<MainView>()
+                }
+            }
+            button("本地联机") {
                 action {
                     replaceWith<MainView>()
                 }
